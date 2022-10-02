a3d03eeac579850a7f0c338e95155a202c7b6da7
Kwon Hyeokbeom <gurqja3810@naver.com>
2022-10-02 16:57:18 +0900

    Program to determine if a number is odd or even

---
+++ c/eod.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void eod(int input)
+  {
+    if(input % 2 == 0)
+      printf("%d is even\n", input);
+
+    else
+      printf("%d is odd\n", input);
+  }
---
+++ c/report1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "libeod.h"
+
+int main(){
+  int input;
+  printf("Input number : ");
+  scanf("%d", &input);
+  eod(input);
+}
